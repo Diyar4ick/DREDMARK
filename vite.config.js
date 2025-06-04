cc94e7bea2cffc852986220afab7bbad69fd386b
Diyar4ick <sabirovdiyar58@gmail.com>
2025-06-04 19:23:17 +0500

    еБл с доменом

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['dredmark.com'], // Добавьте ваш домен
+    allowedHosts: ['Dredmark.com'], // Добавьте ваш домен
   },
 })
